228bfc1b3dcb25f9a5b02dc6930cce9922a1c1fe
ibrahimkholil <ibrahimkholil122@gmail.com>
2021-11-29 23:35:29 +0600

    printf function

---
+++ printf.php
@@ -0,0 +1,20 @@
+<?php
+
+$fname = "ibrahim";
+$lname = "khalil";
+
+/** @var string $fname */
+//printf("My name is %s %s!", $fname,$lname);
+
+$num1 = 123456789;
+$num2 = -123456789;
+$char = 50; // The ASCII Character 50 is 2
+//printf("Binary number = %b ",$num1); // Binary number
+
+//printf("The ASCII Character: %c",$char); // The ASCII Character
+//
+//printf(" Signed decimal number: %d",$num1); // Signed decimal number
+//printf(" Signed decimal number: %d",$num2); // Signed decimal number
+printf("Scientific notation (lowercase):%e",$num1); // Scientific notation (lowercase)
+echo "\n";
+printf("Scientific notation (uppercase):%E",$num1); // Scientific notation (uppercase)
