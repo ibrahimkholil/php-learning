850e2acd784d8125c90b19fb6be8692fc4c02e48
ibrahimkholil <ibrahimkholil122@gmail.com>
2023-01-16 22:43:02 +0600

    recursion function

--- recursion-functions.php
+++ recursion-functions.php
@@ -1,2 +1,12 @@
 <?php
 
+function recusiveNumber($counter,$end,$step = 1){
+        if($counter > $end){
+            return;
+        }
+        echo $counter ."\n";
+        $counter += $step;
+        recusiveNumber($counter,$end,$step);
+}
+
+recusiveNumber(10,27,4);
