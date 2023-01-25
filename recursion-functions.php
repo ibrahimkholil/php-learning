ed1e67bc44a2312b823874dc242b294c07356025
ibrahimkholil <ibrahimkholil122@gmail.com>
2023-01-25 22:41:36 +0600

    factorial by recursion function

--- recursion-functions.php
+++ recursion-functions.php
@@ -1,13 +1,13 @@
 <?php
 // Recrsive function 
-function recusiveNumber($counter,$end,$step = 1){
-        if($counter > $end){
-            return;
-        }
-        echo $counter ."\n";
-        $counter += $step;
-        recusiveNumber($counter,$end,$step);
-}
+// function recusiveNumber($counter,$end,$step = 1){
+//         if($counter > $end){
+//             return;
+//         }
+//         echo $counter ."\n";
+//         $counter += $step;
+//         recusiveNumber($counter,$end,$step);
+// }
 
 // recusiveNumber(10,27,4);
 // fibonacci loop Fn = F(n-1) + F(n-2)
@@ -21,16 +21,24 @@ function recusiveNumber($counter,$end,$step = 1){
 //    $n2 = $temp;
 // }
 // Fibonacci series by recusrive  F(n-1) + F(n-2)
-function fibonacci($n){
-  if ($n <=1) {
-    return $n;
-  }else{
-    return (fibonacci($n-1) + fibonacci($n - 2));
+// function fibonacci($n){
+//   if ($n <=1) {
+//     return $n;
+//   }else{
+//     return (fibonacci($n-1) + fibonacci($n - 2));
    
-  }
+//   }
+// }
+
+// for ($i=0; $i<=15; $i++) {
+//   echo " ". fibonacci( $i );
+// }
+
+function factorial($n){
+    if($n <=1){
+      return 1;
+    }
+    return $n * factorial($n -1);
 }
 
-for ($i=0; $i<=15; $i++) {
-  echo " ". fibonacci( $i );
-}
-
+echo factorial(6);
